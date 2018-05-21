f7e81965bbbb086d5c1c9561ed1ec2848fb3df7e
fridolin /fridolin2 <lars-ole99@gmx.de>
2018-05-21 14:43:56 +0200

    reworked the crafting
    all craftings reworked

--- crafts.lua
+++ crafts.lua
@@ -19,7 +19,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:tree" 3' ,
-	recipe = {"glassmod:frame"}, {"default:tree"}, 
+	recipe = {"glassmod:frame", "default:tree"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -30,7 +30,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:aspen" 3' ,
-	recipe = {"glassmod:frame"}, {"default:aspen_tree"}, 
+	recipe = {"glassmod:frame", "default:aspen_tree"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -41,7 +41,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:acacia" 3' ,
-	recipe = {"glassmod:frame"}, {"default:acacia_tree"}, 
+	recipe = {"glassmod:frame", "default:acacia_tree"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -52,7 +52,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:pine" 3' ,
-	recipe = {"glassmod:frame"}, {"default:pine_tree"}, 
+	recipe = {"glassmod:frame", "default:pine_tree"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -63,7 +63,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:cobble" 3' ,
-	recipe = {"glassmod:frame"}, {"default:cobble"}, 
+	recipe = {"glassmod:frame", "default:cobble"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -74,7 +74,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:stone" 3' ,
-	recipe = {"glassmod:frame"}, {"default:stone"}, 
+	recipe = {"glassmod:frame", "default:stone"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -85,7 +85,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:brick" 3' ,
-	recipe = {"glassmod:frame"}, {"default:brick"}, 
+	recipe = {"glassmod:frame", "default:brick"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -96,7 +96,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:brick_moreblocks" 3' ,
-	recipe = {"glassmod:frame"}, {"default:brick"}, {"default:brick"}, 
+	recipe = {"glassmod:frame", "default:brick"}, {"default:brick"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -107,7 +107,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:chest" 3' ,
-	recipe = {"glassmod:frame"}, {"default:chest"}, 
+	recipe = {"glassmod:frame", "default:chest"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -118,7 +118,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:clay" 3' ,
-	recipe = {"glassmod:frame"}, {"default:clay"}, 
+	recipe = {"glassmod:frame", "default:clay"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -129,7 +129,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:coral_brown" 3' ,
-	recipe = {"glassmod:frame"}, {"default:coral_brown"}, 
+	recipe = {"glassmod:frame", "default:coral_brown"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -140,7 +140,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:coralorange" 3' ,
-	recipe = {"glassmod:frame"}, {"default:coral_orange"}, 
+	recipe = {"glassmod:frame", "default:coral_orange"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -151,7 +151,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:coralskeleton" 3' ,
-	recipe = {"glassmod:frame"}, {"default:coral_skeleton"}, 
+	recipe = {"glassmod:frame", "default:coral_skeleton"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -162,7 +162,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:desertcobble" 3' ,
-	recipe = {"glassmod:frame"}, {"default:desert_cobble"}, 
+	recipe = {"glassmod:frame", "default:desert_cobble"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -173,7 +173,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:dirt" 3' ,
-	recipe = {"glassmod:frame"}, {"default:dirt"}, 
+	recipe = {"glassmod:frame", "default:dirt"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -184,7 +184,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:drygrass" 3' ,
-	recipe = {"glassmod:frame"}, {"default:dirt"}, {"default.dirt"}, 
+	recipe = {"glassmod:frame", "default:dirt"}, {"default.dirt"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -195,7 +195,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:furnace" 3' ,
-	recipe = {"glassmod:frame"}, {"default:furnace"}, 
+	recipe = {"glassmod:frame", "default:furnace"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -206,7 +206,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:grass" 3' ,
-	recipe = {"glassmod:frame"}, {"default:dirt"}, {"default:dirt"}, {"default:sand"},
+	recipe = {"glassmod:frame", "default:dirt"}, {"default:dirt"}, {"default:sand"},
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -217,7 +217,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:gravel" 3' ,
-	recipe = {"glassmod:frame"}, {"default:gravel"}, 
+	recipe = {"glassmod:frame", "default:gravel"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -228,7 +228,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:meselamp" 6' ,
-	recipe = {"glassmod:frame"}, {"default:meselamp"}, 
+	recipe = {"glassmod:frame", "default:meselamp"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -239,7 +239,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:mossycobble" 3' ,
-	recipe = {"glassmod:frame"}, {"default:mossycobble"}, 
+	recipe = {"glassmod:frame", "default:mossycobble"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -250,7 +250,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:silversand" 3' ,
-	recipe = {"glassmod:frame"}, {"default:silver_sand"}, 
+	recipe = {"glassmod:frame", "default:silver_sand"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -261,7 +261,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:snow" 3' ,
-	recipe = {"glassmod:frame"}, {"default:snow"}, 
+	recipe = {"glassmod:frame", "default:snow"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -272,7 +272,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:stoneblock" 3' ,
-	recipe = {"glassmod:frame"}, {"default:stone_block"}, 
+	recipe = {"glassmod:frame", "default:stone_block"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -283,7 +283,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:stonebrick" 3' ,
-	recipe = {"glassmod:frame"}, {"default:stonebrick"}, 
+	recipe = {"glassmod:frame", "default:stonebrick"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -294,7 +294,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:acaciawood" 3' ,
-	recipe = {"glassmod:frame"}, {"default:acacia_wood"}, 
+	recipe = {"glassmod:frame", "default:acacia_wood"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -305,7 +305,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:aspenwood" 3' ,
-	recipe = {"glassmod:frame"}, {"default:aspen_wood"}, 
+	recipe = {"glassmod:frame", "default:aspen_wood"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -316,7 +316,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:junglewood" 3' ,
-	recipe = {"glassmod:frame"}, {"default:junglewood"}, 
+	recipe = {"glassmod:frame", "default:junglewood"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -327,7 +327,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:pinewood" 3' ,
-	recipe = {"glassmod:frame"}, {"default:pine_wood"}, 
+	recipe = {"glassmod:frame", "default:pine_wood"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -338,7 +338,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:treewood" 3' ,
-	recipe = {"glassmod:frame"}, {"default:wood"}, 
+	recipe = {"glassmod:frame", "default:wood"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -349,7 +349,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:bronze" 4' ,
-	recipe = {"glassmod:frame"}, {"default:bronze_ingot"}, 
+	recipe = {"glassmod:frame", "default:bronze_ingot"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -360,7 +360,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:coal" 4' ,
-	recipe = {"glassmod:frame"}, {"default:coal_lump"}, 
+	recipe = {"glassmod:frame", "default:coal_lump"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -371,7 +371,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:copper" 4' ,
-	recipe = {"glassmod:frame"}, {"default:copper_ingot"}, 
+	recipe = {"glassmod:frame", "default:copper_ingot"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -382,7 +382,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:desertstone" 3' ,
-	recipe = {"glassmod:frame"}, {"default:desert_stone"}, 
+	recipe = {"glassmod:frame", "default:desert_stone"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -393,7 +393,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:desertsand" 3' ,
-	recipe = {"glassmod:frame"}, {"default:desert_sand"}, 
+	recipe = {"glassmod:frame", "default:desert_sand"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -404,7 +404,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:desertstonebrick" 3' ,
-	recipe = {"glassmod:frame"}, {"default:desert_stonebrick"}, 
+	recipe = {"glassmod:frame", "default:desert_stonebrick"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -415,7 +415,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:desertstoneblock" 3' ,
-	recipe = {"glassmod:frame"}, {"default:desert_stone_block"}, 
+	recipe = {"glassmod:frame", "default:desert_stone_block"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -426,7 +426,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:diamond" 11' ,
-	recipe = {"glassmod:frame"}, {"default:diamond"}, 
+	recipe = {"glassmod:frame", "default:diamond"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -437,7 +437,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:gold" 6' ,
-	recipe = {"glassmod:frame"}, {"default:gold_ingort"}, 
+	recipe = {"glassmod:frame", "default:gold_ingort"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -448,7 +448,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:jungle_tree" 3' ,
-	recipe = {"glassmod:frame"}, {"default:jungletree"}, 
+	recipe = {"glassmod:frame", "default:jungletree"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -459,7 +459,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:mese" 11' ,
-	recipe = {"glassmod:frame"}, {"default:mese_crystal"}, 
+	recipe = {"glassmod:frame", "default:mese_crystal"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -470,7 +470,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:sand" 3' ,
-	recipe = {"glassmod:frame"}, {"default:sand"}, 
+	recipe = {"glassmod:frame", "default:sand"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -481,7 +481,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:sandstone" 3' ,
-	recipe = {"glassmod:frame"}, {"default:sandstone"}, 
+	recipe = {"glassmod:frame", "default:sandstone"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -492,7 +492,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:sandstoneblock" 3' ,
-	recipe = {"glassmod:frame"}, {"default:sandstone_block"}, 
+	recipe = {"glassmod:frame", "default:sandstone_block"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -503,7 +503,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:sandstonebrick" 3' ,
-	recipe = {"glassmod:frame"}, {"default:sandstonebrick"}, 
+	recipe = {"glassmod:frame", "default:sandstonebrick"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -514,7 +514,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:steel" 6' ,
-	recipe = {"glassmod:frame"}, {"default:steel_ingot"}, 
+	recipe = {"glassmod:frame", "default:steel_ingot"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -525,7 +525,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:ice" 3' ,
-	recipe = {"glassmod:frame"}, {"default:ice"}, 
+	recipe = {"glassmod:frame", "default:ice"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -536,7 +536,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:treeleaves" 3' ,
-	recipe = {"glassmod:frame"}, {"default:leaves"}, 
+	recipe = {"glassmod:frame", "default:leaves"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -547,7 +547,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:pineneedles" 3' ,
-	recipe = {"glassmod:frame"}, {"default:pine_needles"}, 
+	recipe = {"glassmod:frame", "default:pine_needles"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -558,7 +558,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:aspenleaves" 3' ,
-	recipe = {"glassmod:frame"}, {"default:aspen_leaves"}, 
+	recipe = {"glassmod:frame", "default:aspen_leaves"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -569,7 +569,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:acacialeaves" 3' ,
-	recipe = {"glassmod:frame"}, {"default:acacia_leaves"}, 
+	recipe = {"glassmod:frame", "default:acacia_leaves"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -580,7 +580,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:jungleleaves" 3' ,
-	recipe = {"glassmod:frame"}, {"default:jungleleaves"}, 
+	recipe = {"glassmod:frame", "default:jungleleaves"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -591,7 +591,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:cactusside" 3' ,
-	recipe = {"glassmod:frame"}, {"default:cactus"}, {"default:sand"}, 
+	recipe = {"glassmod:frame", "default:cactus", "default:sand"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -602,7 +602,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:cactustop" 3' ,
-	recipe = {"glassmod:frame"}, {"default:cactus"}, {"default:dirt"}, 
+	recipe = {"glassmod:frame", "default:cactus", "default:dirt"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -613,7 +613,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:water" 4' ,
-	recipe = {"glassmod:frame"}, {"dye:blue"}, {"dye:blue"}, 
+	recipe = {"glassmod:frame", "dye:blue", "dye:blue"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -624,7 +624,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:riverwater" 4' ,
-	recipe = {"glassmod:frame"}, {"dye:cyan"}, {"dye:cyan"}, 
+	recipe = {"glassmod:frame", "dye:cyan", "dye:cyan"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -635,7 +635,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:lava" 3' ,
-	recipe = {"glassmod:frame"}, {"dye:red"}, {"dye:orange"}
+	recipe = {"glassmod:frame", "dye:red", "dye:orange"}
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -646,7 +646,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:bookshelf" 3' ,
-	recipe = {"glassmod:frame"}, {"default:bookshelf"}, 
+	recipe = {"glassmod:frame", "default:bookshelf"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
@@ -657,7 +657,7 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = '"glassmod:obsidian" 11' ,
-	recipe = {"glassmod:frame"}, {"default:obsidian"}, 
+	recipe = {"glassmod:frame", "default:obsidian"}, 
 })
 minetest.register_craft( {
 	type = "shapeless",
